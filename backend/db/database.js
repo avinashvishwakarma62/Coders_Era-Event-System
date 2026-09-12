b5ba97a29e6a09eec04900bb6ef21c5d78f8403c
Avinash Vishwakarma <avinashvishwakarma163@gmail.com>
2026-09-12 10:33:11 +0530

    Fix Vercel frontend and backend routing

--- backend/db/database.js
+++ backend/db/database.js
@@ -7,7 +7,11 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT) || 5432
+    port: Number(process.env.DB_PORT) || 5432,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on("connect", () => {
